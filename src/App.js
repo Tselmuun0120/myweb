0f3e4bdbd94ba3eb1fd477f3aa76219a73ec579a
Tseku <67298502+Tselmuun0120@users.noreply.github.com>
2020-09-02 17:47:08 +0800

    ts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import image1 from './image1.jpg';
+import './App.scss';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={image1} className="App-image" alt="image" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The people who are crazy enough to think they can change the world are the ones who do.
         </p>
-        <a
+        <h1>
+          Timeline
+        </h1>
+      <span>24 Feb Steven Paul was born in San Francisco, the son of Abdulfattah Jandali and Joanne Schieble. 
+          He is quickly adopted by Paul and Clara Jobs </span>
+          <span>1969 Steve Jobs meets Steve Wozniak, 5 years older, through a mutual friend. Woz and Steve share a love of electronics, Bob Dylan, and pranks</span>
+  
+        
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          TSEKU
-        </a>
+        </a> */}
       </header>
     </div>
+   
   );
 }
 

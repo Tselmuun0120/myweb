fb0d3d401ed4e508c941ad45b0074ccc8469fb46
Tseku <67298502+Tselmuun0120@users.noreply.github.com>
2020-10-16 09:37:46 +0800

    tse

--- src/component/homework3/App3.js
+++ src/component/homework3/App3.js
@@ -2,17 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App3.scss";
 import "swiper/swiper.scss";
+// import Swiper, { Navigation } from "swiper";
+import Swiper from "react-id-swiper";
+import { Maindata } from "./data";
 
-export const Navigation = [
-  { menu: "NEW" },
-  { menu: "WOMEN" },
-  { menu: "MEN" },
-  { menu: "HOME" },
-  { menu: "ACCESSORIES" },
-  { menu: "OUR PRODUCTION" },
-  { menu: "SALE" },
-];
 const App3 = () => {
+  const params = {
+    slidesPerView: 4,
+    spaceBetween: 20,
+  };
+
+  const param = {
+    slidesPerView: 4,
+    spaceBetween: 20,
+  };
   return (
     <div className="Big">
       <header>
@@ -41,7 +44,7 @@ const App3 = () => {
               <div className="sign"></div>
             </div>
             <div className="menus">
-              {Navigation.map((list, index) => {
+              {Maindata.Navigation.data.map((list, index) => {
                 return (
                   <div className="cmenu">
                     <a href="#">{list.menu}</a>
@@ -113,7 +116,49 @@ const App3 = () => {
           </div>
         </div>
       </section>
-      <section className="dresses"></section>
+      <section className="women">
+        <div className="container">
+          <div className="text">
+            <h1>Dresses</h1>
+          </div>
+          <Swiper {...params}>
+            {Maindata.women.data.map((list, index) => {
+              return (
+                <div className="images">
+                  <img src={require(`../../assets/dress/${list.image}`)} />
+                  <div className="descr">
+                    <h1>{list.name}</h1>
+                    <h3>{list.price}</h3>
+                    <span>{list.color}</span>
+                  </div>
+                </div>
+              );
+            })}
+          </Swiper>
+        </div>
+      </section>
+      <section className="men">
+        <div className="container">
+          <div className="text">
+            <h1>Printed Scarves</h1>
+          </div>
+
+          <Swiper {...param}>
+            {Maindata.men.data.map((list, index) => {
+              return (
+                <div className="images">
+                  <img src={require(`../../assets/scarf/${list.image}`)} />
+                  <div className="descr">
+                    <h1>{list.name}</h1>
+                    <h3>{list.price}</h3>
+                    <span>{list.color}</span>
+                  </div>
+                </div>
+              );
+            })}
+          </Swiper>
+        </div>
+      </section>
       <section className="instagram">
         <div className="container">
           <div className="gobi">
@@ -169,6 +214,87 @@ const App3 = () => {
                   className="zurag"
                   src={require("../../assets/cashmere/cashmere2.png")}
                 />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="footer">
+        <div className="container">
+          <div className="grids">
+            <div className="grid1">
+              <h1>{Maindata.about.name}</h1>
+              <ul>
+                {Maindata.about.data.map((list, index) => {
+                  return (
+                    <li>
+                      <a href={list.link}>{list.item}</a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="grid2">
+              <h1>{Maindata.help.name}</h1>
+              <ul>
+                {Maindata.help.data.map((list, index) => {
+                  return (
+                    <li>
+                      <a href={list.link}>{list.item}</a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="grid3">
+              <h1>{Maindata.social.name}</h1>
+              <ul>
+                {Maindata.social.data.map((list, index) => {
+                  return (
+                    <li>
+                      <a href={list.link}>
+                        <i className={list.icon}></i>
+                        {list.title}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="grid4">
+              <div className="subs">
+                <h1>Join our list</h1>
+                <div className="para">
+                  <p>
+                    Subscribe to get special offers, free giveaways, and
+                    once-in-a-lifetime deals.
+                  </p>
+                </div>
+
+                <form>
+                  <div class="sub">
+                    <input
+                      type="email"
+                      placeholder="Enter your email"
+                      className="form-control"
+                    />
+                    <button className="btn btn-primary">
+                      SUBSCRIBE
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="15"
+                        height="12"
+                        viewBox="0 0 6 9"
+                        fill="none"
+                      >
+                        <path
+                          d="M0.602539 0.490723L4.66504 4.55322L0.602539 8.61572"
+                          stroke="black"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
---
+++ src/component/homework3/data.js
@@ -0,0 +1,172 @@
+export const Maindata = {
+  Navigation: {
+    data: [
+      { menu: "NEW" },
+      { menu: "WOMEN" },
+      { menu: "MEN" },
+      { menu: "HOME" },
+      { menu: "ACCESSORIES" },
+      { menu: "OUR PRODUCTION" },
+      { menu: "SALE" },
+    ],
+  },
+  women: {
+    data: [
+      {
+        name: "BOAT NECK DRESS",
+        price: "$350",
+        color: "+6 Colors",
+        image: "dresses1.png",
+      },
+
+      {
+        name: "BOAT NECK DRESS",
+        price: "$330",
+        color: "+6 Colors",
+        image: "dresses2.png",
+      },
+
+      {
+        name: "BOAT NECK DRESS",
+        price: "$300",
+        color: "+6 Colors",
+        image: "dresses1.png",
+      },
+
+      {
+        name: "BOAT NECK DRESS",
+        price: "$350",
+        color: "+6 Colors",
+        image: "dresses2.png",
+      },
+
+      {
+        name: "BOAT NECK DRESS",
+        price: "$350",
+        color: "+6 Colors",
+        image: "dresses1.png",
+      },
+
+      {
+        name: "BOAT NECK DRESS",
+        price: "$380",
+        color: "+6 Colors",
+        image: "dresses2.png",
+      },
+    ],
+  },
+
+  men: {
+    data: [
+      {
+        name: "PRINTED SCARVES",
+        price: "$350",
+        color: "+6 Colors",
+        image: "scarf1.png",
+      },
+
+      {
+        name: "PRINTED SCARVES",
+        price: "$330",
+        color: "+6 Colors",
+        image: "scarf2.png",
+      },
+
+      {
+        name: "PRINTED SCARVES",
+        price: "$300",
+        color: "+6 Colors",
+        image: "scarf1.png",
+      },
+
+      {
+        name: "PRINTED SCARVES",
+        price: "$350",
+        color: "+6 Colors",
+        image: "scarf2.png",
+      },
+
+      {
+        name: "PRINTED SCARVES",
+        price: "$350",
+        color: "+6 Colors",
+        image: "scarf1.png",
+      },
+
+      {
+        name: "PRINTED SCARVES",
+        price: "$380",
+        color: "+6 Colors",
+        image: "scarf2.png",
+      },
+    ],
+  },
+
+  about: {
+    name: "БИДНИЙ ТУХАЙ",
+    data: [
+      {
+        item: "Хүний нөөцийн тухай",
+        link: "https://www.gobicashmere.com/pages/our-history",
+      },
+      {
+        item: "Манай баг",
+        link: "https://www.gobicashmere.com/pages/our-history",
+      },
+      {
+        item: "Хүний нөөцийн тухай",
+        link: "https://www.gobicashmere.com/pages/our-history",
+      },
+      {
+        item: "Ажлын байр",
+        link: "https://www.gobicashmere.com/pages/our-history"
+      }
+    ],
+  },
+  help: {
+    name: "ТУСЛАЛЦАА",
+    data: [
+      {
+        item: "Хүний нөөцийн тухай",
+        link: "https://www.gobicashmere.com/pages/our-history",
+      },
+      {
+        item: "Мэдээ Мэдээлэл",
+        link: "https://www.gobicashmere.com/pages/our-history",
+      },
+      {
+        item: "Салбарын байршил",
+        link: "https://www.gobicashmere.com/pages/our-history",
+      },
+      {
+        item: "Холбоо барих",
+        link: "https://www.gobicashmere.com/pages/our-history",
+      },
+    ],
+  },
+  social: {
+    name: "ХОЛБОО БАРИХ",
+    data: [
+      {
+        icon: "fa fa-facebook-f",
+        link: "https://www.facebook.com/",
+        title: "Facebook",
+      },
+      {
+        icon: "fa fa-instagram",
+        link: "https://www.instagram.com/",
+        title: "Instagram",
+      },
+      {
+        icon: "fa fa-twitter",
+        link: "https://www.twitter.com/",
+        title: "Twitter",
+      },
+      {
+        icon: "fa fa-youtube",
+        link: "https://www.youtube.com/",
+        title: "Youtube",
+      },
+    ],
+  },
+};
